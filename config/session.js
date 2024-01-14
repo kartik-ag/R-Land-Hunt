8bbd4b9d68e71faef7b9e58b43073e3b63cd54ba
Kartik <agarwalkartik2018@gmail.com>
2024-01-14 18:24:27 +0530

    deploy

--- config/session.js
+++ config/session.js
@@ -3,7 +3,7 @@ const expressSession = require('express-session');
 function createSessionStore() {
     const MongoDBstore = mongoDbStore(expressSession);
     const store = new MongoDBstore({
-        uri: process.env.MONGODB_URI,
+        uri: 'mongodb+srv://agarwalkartik2018:4U2rDmJ9jtrtu8BO@rlandhunt.fpk6bx6.mongodb.net/?retryWrites=true&w=majority',
         collection: 'sessions',
         databaseName: 'hunt'
     });
