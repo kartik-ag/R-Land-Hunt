c3f91238a7dbf2f8877e50d17c6e50d9be2c45c5
mayankdwiv <122630722+mayankdwiv@users.noreply.github.com>
2024-01-30 13:25:12 +0530

    Modified database.js file

--- data/database.js
+++ data/database.js
@@ -3,30 +3,12 @@ require('dotenv').config();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+let database;
 
-async function run() {
-    const uri = "mongodb://0.0.0.0:27017";
-
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+async function connectToDatabase() {
+    const client = await MongoClient.connect('mongodb://0.0.0.0:27017');
+    database = client.db('hunt');
 }
-run().catch(console.dir);
 
 function getDb() {
     if (!database) {
@@ -36,7 +18,7 @@ function getDb() {
 }
 
 module.exports = {
-    connectToDatabase: run,
+    connectToDatabase: connectToDatabase,
     getDb: getDb
 };
 
